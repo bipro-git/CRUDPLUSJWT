6403258d233c9a80144c1f003f2dd5a47ba47fe6
bipro-git <biprodep.das@indusnet.co.in>
2024-07-15 11:56:17 +0530

    Added some modifications in custom middleware

--- CRUD_PRACT/CustomMiddleware.cs
+++ CRUD_PRACT/CustomMiddleware.cs
@@ -14,11 +14,13 @@ namespace CRUD_PRACT
         public async Task InvokeAsync(HttpContext context)
         {
             var request = context.Request;
-            var logDetails = $"Timestamp: {DateTime.Now}\n" +
+            var logDetails = $"\n\n\nTimestamp: {DateTime.Now}\n" +
                          $"Method: {request.Method}\n" +
                          $"Path: {request.Path}\n" +
-                         $"QueryString: {request.QueryString}\n" +
-                         $"Headers: {request.Headers}\n\n\n\n";
+                         $"QueryString: {request.QueryString.ToString()}\n" +
+                         $"Headers: {request.Headers}\n"+
+                         $"Body : {request.Body}";
+            
 
             await File.AppendAllTextAsync("RequestLog.txt", logDetails);
             await next(context);
